9382356e7a2602dfb24be9ff044ca97a48a26188
zengn <zengn@xiaopeng.com>
2024-09-06 21:19:14 +0800

    add delete feat

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import {ref, onMounted} from 'vue'
 
 // TODO: 列表渲染
 // 思路:声明响应式list->调用接口获取数据->后端数据赋值给list->绑定到table组件
-
 const list = ref([])
 const getList = async () =>{
   const res = await axios.get('/list')
@@ -13,9 +12,11 @@ const getList = async () =>{
 }
 onMounted(() => getList())
 
-
-
 // TODO: 删除功能
+const onDelete = async(id)=>{
+  await axios.delete(`/del/${id}`)
+  getList()
+}
 
 
 // TODO: 编辑功能
@@ -29,9 +30,9 @@ onMounted(() => getList())
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default="{row}">
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" link @click="onDelete(row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
