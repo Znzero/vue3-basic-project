f846b775af0ed5888a1f3befcb6517da1943df85
zengn <zengn@xiaopeng.com>
2024-09-10 18:20:34 +0800

    add edit feat

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ const getList = async () =>{
 onMounted(() => getList())
 
 // TODO: 删除功能
+// 思路：获取当前行id->通过id调用删除接口->调用get接口获取最新列表
 const onDelete = async(id)=>{
   await axios.delete(`/del/${id}`)
   getList()
@@ -20,6 +21,21 @@ const onDelete = async(id)=>{
 
 
 // TODO: 编辑功能
+// 思路:打开弹框->回填数据>更新数据
+// 1.打开弹框(获取子组件实例调用方法或者修改属性)
+// 2.回调数据(调用详情接口/当前行的静态数据)
+
+const editRef = ref(null)
+const openDialog = (item) => {
+  console.log('item', item)
+  editRef.value.open(item)
+}
+
+
+//更新列表
+const updateList = () =>{
+  getList()
+}
 
 </script>
 
@@ -31,13 +47,13 @@ const onDelete = async(id)=>{
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
         <template #default="{row}">
-          <el-button type="primary" link>编辑</el-button>
+          <el-button type="primary" link @click="openDialog(row)">编辑</el-button>
           <el-button type="danger" link @click="onDelete(row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="editRef" @update-list="updateList" />
 </template>
 
 <style scoped>
